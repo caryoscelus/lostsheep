05fb662b0b752924f009be4fc70018e92243efbe
caryoscelus <caryoscelus@gmx.com>
2013-09-01 19:27:56 +0400

    a bit of scripting..

--- data/scripts/story.clj
+++ data/scripts/story.clj
@@ -1,3 +1,23 @@
 (ns story)
 
-(bind "test" (t "Hello"))
+(def story-stage :contact)
+
+(def know-mechanic false)
+
+(bind "mechanic"
+      (fn []
+          (cond
+              (not know-mechanic) (run "mechanic-contact")
+              :else (run "mechanic-empty"))))
+
+(bind "mechanic-contact" (t "
+[Mechanic description]
+"
+(list
+    (ln "..." nil))))
+
+(bind "mechanic-empty" (t "
+I approached mechanic..
+"
+(list
+    (ln "..." nil))))
