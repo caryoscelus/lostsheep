e024fc41140c2ff5e3a0c6f068379b09e8cc4cb4
caryoscelus <caryoscelus@gmx.com>
2013-09-07 19:55:08 +0400

    a bit of story

--- data/scripts/story.clj
+++ data/scripts/story.clj
@@ -114,8 +114,38 @@
            :else (run (t "can't go down for unknown reason.."))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(bind "welcome"
-      (t "Wilkamen!"))
+(bindf "welcome"
+       (run "day0-0-contact"))
+
+(bind "day0-0-contact"
+      (t "..I try to open my eyes, but can't see anything. I try to move my limbs, but that doesn't work.^^
+         I'm lieing on hard surface, trying to remember what happend. I can't..^^
+         
+         Eventually, darkness starts to go away. I can see the sky, clear blue sky. Morning sky. And i can
+         hear water noise. Like i'm on the sea shore.. I try to sit up again and what i see is..
+         i can't trust my eyes, this must be a dream! How can i happen to be on some weird, rusty ship in the
+         middle of the ocean?!..^^
+         
+         When i admit this fact to myself, i try to remember what happend. Just yestarday evening, i was
+         drinking in the company of my college friends, celebrating anniversary of graduation.. Then i went
+         home. I can even remember how i went to bed! What on earth could've happend afterwards?!.."
+         
+         (list
+             (ln "Look around"
+                 (t "Anyway, i can't help myself just trying to remember what happend. It is in the past already
+                    and i should think about future. I must learn my current situation and decide what to do
+                    next.^^
+                    With such thoughts, i started looking around. And suddenly i noticed that i'm not alone here!"))
+             
+             (ln "Keep trying to recall"
+                 (t "I can't do anything without knoledge of situation. I must remember...^^
+                    
+                    There was this weird dream, about.. eh, elephants?.. Yeah, elephants; and i was riding one
+                    of them. And when we came to our destination point, i awakend. I think i went to the kitchen
+                    and drunk some water.. Was it my kitchen? Wait, was i really awake at that point? Or that
+                    was just continuation of my dream?..^^
+                    
+                    Suddenly, my thoughts were interrupted by some sound. Like a human voice!..")))))
 
 (bind "day0-1-investigate-ship"
       (t "So we decided to look at ship.."))
@@ -128,8 +158,12 @@
 (bindf "go-down"
        (change-map "cabins"))
 
+(def inside-control-room false)
+
 (bindf "go-control"
-       (self-move -1 0))
+       (if inside-control-room
+           (do (def inside-control-room false) (self-move 1 0))
+           (do (def inside-control-room true) (self-move -1 0))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; called on init
